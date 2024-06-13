014b348a5d66752caca0c086c63ef8af896a068b
qingkongg <2241966271@qq.com>
2024-06-13 06:17:24 -0700

    first

--- src/simulator.c
+++ src/simulator.c
@@ -18,7 +18,7 @@ bool is_virtual_page_allocated(Process *process, addr_t address,size_t l1_index,
   if (!process->page_table.entries[l1_index].entries[l2_index].valid) {
     return true;  // 如果L2页表项无效，返回false
   }
-  return false;  // 虚拟页面已被分配，返回true
+  return true;  // 虚拟页面已被分配，返回true
 }
 
 status_t allocate_page(Process *process, addr_t address, addr_t physical_address) {
