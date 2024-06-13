b16ca97401c9ff3f91cd36fc357bb779a545e791
qingkongg <2241966271@qq.com>
2024-06-13 06:08:40 -0700

    first

--- src/simulator.c
+++ src/simulator.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include<stdbool.h>
 
+
 // 检查虚拟页面是否已分配的辅助函数
 bool is_virtual_page_allocated(Process *process, addr_t address,size_t l1_index,size_t l2_index) {
   // 检查L1页表项是否有效
@@ -11,11 +12,11 @@ bool is_virtual_page_allocated(Process *process, addr_t address,size_t l1_index,
 
   }
   if (!process->page_table.entries[l1_index].entries) {
-    return true;  // 如果L1页表项无效，返回false
+    return false;  // 如果L1页表项无效，返回false
   }
   // 检查L2页表项是否有效
   if (!process->page_table.entries[l1_index].entries[l2_index].valid) {
-    return true;  // 如果L2页表项无效，返回false
+    return false;  // 如果L2页表项无效，返回false
   }
   return true;  // 虚拟页面已被分配，返回true
 }
@@ -35,7 +36,7 @@ status_t allocate_page(Process *process, addr_t address, addr_t physical_address
   // 检查物理内存是否足够
   
   // 检查虚拟页面是否已分配
-  if (is_virtual_page_allocated(process, address,l1_index,l2_index)) {
+  if (!is_virtual_page_allocated(process, address,l1_index,l2_index)) {
     return ERROR; 
   }
 
